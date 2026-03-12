a2cac01cf45e5781f9e3120b4d616d534ca315da
danuwarisman <danuwarisman514@gmail.com>
2026-03-12 23:05:29 +0700

    tm

---
+++ 03_GUI_dengan_HTML_dan_CSS/TM/index.js
@@ -0,0 +1,40 @@
+const editorElement = document.getElementById("editor-kecil");
+const charCountElement = document.getElementById("hf");
+const hkElement = document.getElementById("hk");
+const hbElement = document.getElementById("hb");
+
+function updateHitungan() {
+    let teks = editorElement.value;
+    charCountElement.textContent = teks.length;
+    
+    let jmlKecil = 0;
+    let jmlBesar = 0;
+    
+    // Looping ngecek satu satu huruf nya
+    for(let i=0; i < teks.length; i++){
+        if(teks[i] >= 'a' && teks[i] <= 'z'){
+            jmlKecil++;
+        } else if(teks[i] >= 'A' && teks[i] <= 'Z'){
+            jmlBesar++;
+        }
+    }
+    
+    hkElement.textContent = jmlKecil;
+    hbElement.textContent = jmlBesar;
+}
+
+// Update pas lagi ngetik
+editorElement.addEventListener("input", updateHitungan);
+
+const btnBesar= document.getElementById("huruf-besar");
+const btnKecil = document.getElementById("huruf-kecil");
+
+btnBesar.addEventListener("click", function() {
+   editorElement.value = editorElement.value.toUpperCase();
+   updateHitungan(); // Biar angkanya langsung nyesuaiin pas diklik
+});
+
+btnKecil.addEventListener("click", function() {
+    editorElement.value = editorElement.value.toLowerCase();
+    updateHitungan();
+});
